17b62731bc9932392ea7cc93b2ee900d9704e65f
Rithik Aggarwal <rithika@umich.edu>
2020-12-05 15:46:56 -0500

    data collection and simple algo by rithik and logan

--- backtesting/testing_algos/mean_reversion.py
+++ backtesting/testing_algos/mean_reversion.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from papertrading.get_account_data import place_order
 
 class MeanReversion:
     def __init__(self, ticker):
@@ -7,29 +8,58 @@ class MeanReversion:
         self.today_30_day = 0
         self.today_90_day = 0
         #self.place_order = place_order
-        #self.get_data = get_data
         self.ticker = ticker
         
 
     def handler(self, new_data):
-        #RUN THE LOGIC HANDLER HERE
-        pass
+        if (self.today_30_day > self.today_90_day and self.yesterday_30_day < self.yesterday_90_day):
+            print('trigger sell')
+            #place_order(self.ticker, 1, 'sell')
+        elif (self.today_30_day < self.today_90_day and self.yesterday_30_day > self.yesterday_90_day):
+            print('trigger buy')
+            #place_order(self.ticker, 1, 'buy')
+        else:
+            print('do nothing')
+            #do nothing
+        
 
     def get_new_data(self):
         filename = "../../data/historical_data/" + self.ticker + ".csv"
         #print(filename)
-        df = pd.read_csv(filename, nrows=90)
-        print(df)
-        print(df['Close'])
-        pass
-        #return self.get_data([self.ticker], ["Close"])
-        for close in df['Close']:
 
-        for index,row in df.iterrows():
-            row['Close']
+        #calculate 90 day average today
+        df = pd.read_csv(filename, nrows=90)
+        for close in df['Close']:
+            self.today_90_day += close
+        self.today_90_day /= 90
+
+        #calculate 90 day average yesterday
+        df = pd.read_csv(filename,skiprows=[1] ,nrows=90)
+        for close in df['Close']:
+            self.yesterday_90_day += close
+        self.yesterday_90_day /= 90
+
+        #calculate 30 day average today
+        df = pd.read_csv(filename, nrows=30)
+        for close in df['Close']:
+            self.today_30_day += close
+        self.today_30_day /= 30
+
+        #calculate 30 day average yesterday
+        df = pd.read_csv(filename,skiprows=[1] ,nrows=30)
+        for close in df['Close']:
+            self.yesterday_30_day += close
+        self.yesterday_30_day /= 30
+
+        results = {'t90': self.today_90_day, 't30': self.today_30_day, 'y90': self.yesterday_90_day, 'y30': self.yesterday_30_day}
+
+        return results
 
 
 if __name__ == "__main__":
     meanreversion = MeanReversion('AAPL')
-    #backtester.get_new_data()
     print(meanreversion.get_new_data())
+
+#Questions:
+#   1. If we calcualte the 90 day averages with the close, then we wont be able to trigger a buy/sell until the next day
+#       a. do we calculate with opens or just hope that it goes through the next day?
